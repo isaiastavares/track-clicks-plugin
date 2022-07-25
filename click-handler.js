5849c50a6d746ba0afa11d0cd9ba7fb009b94c42
Isaias Tavares <isaiasengsoft@gmail.com>
2022-07-25 16:44:10 -0300

    fix event click with ip

--- click-handler.js
+++ click-handler.js
@@ -2,32 +2,35 @@ document.addEventListener(`click`, e => {
     const origin = e.target.closest(`a`);
     const targetUrl = new URL(origin.href)
     if (origin && location.hostname != targetUrl.hostname) {
-        var ip = ""
         $.ajax({
             type : "GET",
             url : "https://api.ipify.org?format=json",
             crossDomain: true,
             contentType: "application/json",
             success: function (result) {
-                ip = result.ip
+                console.log(result)
+                eventClick(origin.href, result.ip)
             },
             error: function (result, status) {
                 alert(result)
-            }
-        });
-        alert(ip)
-        $.ajax({
-            type: "POST",
-            url: "https://select-software-reviews.bubbleapps.io/version-live/api/1.1/wf/trackclicks",
-            data: JSON.stringify({ "url": origin.href, "ip": ip }),
-            crossDomain: true,
-            contentType: "application/json",
-            success: function (result) {
-                console.log(result);
-            },
-            error: function (result, status) {
-                alert(result)
+                eventClick(origin.href, null)
             }
         });
     }
 });
+
+function eventClick(url, ip) {
+    $.ajax({
+        type: "POST",
+        url: "https://select-software-reviews.bubbleapps.io/version-live/api/1.1/wf/trackclicks",
+        data: JSON.stringify({ "url": origin.href, "ip": ip }),
+        crossDomain: true,
+        contentType: "application/json",
+        success: function (result) {
+            console.log(result);
+        },
+        error: function (result, status) {
+            alert(result)
+        }
+    });
+}
