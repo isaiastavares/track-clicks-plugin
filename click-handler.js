af3d71059b6b4bf9995784f7f780967a06444960
Isaias Tavares <isaiasengsoft@gmail.com>
2022-07-25 16:28:57 -0300

    add tracking ip

--- click-handler.js
+++ click-handler.js
@@ -2,10 +2,14 @@ document.addEventListener(`click`, e => {
     const origin = e.target.closest(`a`);
     var targetUrl = new URL(origin.href)
     if (origin && location.hostname != targetUrl.hostname) {
+        $.getJSON("https://api.ipify.org?format=json", function(data) {
+            const ip = data.ip
+        })
+        alert(ip)
         $.ajax({
             type: "POST",
             url: "https://select-software-reviews.bubbleapps.io/version-live/api/1.1/wf/trackclicks",
-            data: JSON.stringify({ "url": origin.href }),
+            data: JSON.stringify({ "url": origin.href, "ip": ip }),
             crossDomain: true,
             contentType: "application/json",
             success: function (result) {
